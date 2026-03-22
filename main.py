a499275939eb4cd281c12bc50481fcf20c835cfb
Finnerdespin <finn.rijnhout@gmail.com>
2026-03-22 07:56:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from python import gui
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+if __name__ == "__main__":
+    gui.start_gui()
